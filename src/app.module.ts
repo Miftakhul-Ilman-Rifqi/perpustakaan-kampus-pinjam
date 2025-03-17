c89d848ac9da32a9533b4e01feea376ba810932b
Miftakhul Ilman Rifqi <bozzclax@gmail.com>
2025-03-17 13:30:20 +0700

    Add LoanModule to app imports for loan management integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { StudentModule } from './student/student.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AbilitiesGuard } from './common/casl/abilities.guard';
 import { BookModule } from './book/book.module';
+import { LoanModule } from './loan/loan.module';
 
 @Module({
-  imports: [CommonModule, SuperadminModule, StudentModule, BookModule],
+  imports: [
+    CommonModule,
+    SuperadminModule,
+    StudentModule,
+    BookModule,
+    LoanModule,
+  ],
   controllers: [],
   providers: [
     {
