6d3af7d9123cd0727212bd48f365b54fa1da4375
Miftakhul Ilman Rifqi <bozzclax@gmail.com>
2025-03-20 01:04:23 +0700

    Comment out custom CSS and JS configuration for Swagger UI in main.ts for potential debugging

--- src/main.ts
+++ src/main.ts
@@ -104,12 +104,12 @@ async function bootstrap() {
     swaggerOptions: {
       defaultModelsExpandDepth: -1, // This will hide the schemas section
     },
-    customCssUrl:
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-    customJs: [
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
-      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
-    ],
+    // customCssUrl:
+    //   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    // customJs: [
+    //   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+    //   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    // ],
   });
 
   await app.listen(process.env.PORT ?? 3000);
